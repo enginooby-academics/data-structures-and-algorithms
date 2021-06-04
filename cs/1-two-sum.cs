26b05c2f8130b63a83fd9e005918f7a4a6ba6336
unknown <enginoobz@gmail.com>
2021-06-05 01:42:40 +0200

    1. Two Sum: C# | Two-pass hash table/dictionary

--- cs/1-two-sum.cs
+++ cs/1-two-sum.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 class TwoSum {
         static void Main(string[] args) {
-                int[] nums = new int[4] { 2, 5, 3, 14 };
+                // int[] nums = new int[5] { 2, 5, 46, 14, 9 };
+                int[] nums = new int[2] { 3, 3 };
                 int target = 11;
 
-                int[] result = TwoSumBruteForce(nums, target);
+                int[] result = TwoSumTwoPassHashtable(nums, target);
                 Array.ForEach(result, Console.WriteLine);
         }
 
-        //Time: O(n^2)
+        // O(n^2) time - O(1) space
         public static int[] TwoSumBruteForce(int[] nums, int target) {
                 int size = nums.Length;
                 int[] indices = new int[2];
@@ -26,4 +27,74 @@ class TwoSum {
 
                 throw new ArgumentException("Not found");
         }
+
+        // O(n) time & space
+        public static int[] TwoSumTwoPassHashtable(int[] nums, int target) {
+                int size = nums.Length;
+                int[] indices;
+                var nums_hash = new System.Collections.Hashtable(); // num & index
+                // var nums_hash = new System.Collections.Generic.Dictionary<int, int>();
+
+                // generate hash table from array
+                for (int i = 0; i < size; i++) {
+                        int num = nums[i];
+
+                        if (!nums_hash.ContainsKey(num)) {
+                                nums_hash.Add(num, i);
+                                continue;
+                        }
+
+                        // corner case: duplicated key
+                        if (num == target / 2) {
+                                indices = new int[2] { (int)nums_hash[num], i };
+                                return indices;
+                        }
+                }
+
+                // find the complement
+                foreach (int num in nums_hash.Keys) {
+                        int complement = target - num;
+                        if (nums_hash.ContainsKey(complement) && nums_hash[complement] != nums_hash[num]) {
+                                indices = new int[2] { (int)nums_hash[num], (int)nums_hash[complement] };
+                                return indices;
+                        }
+                }
+
+                throw new ArgumentException("Not found");
+        }
+
+        // O(n) time & space
+        public static int[] TwoSumTwoPassDictionary(int[] nums, int target) {
+                int size = nums.Length;
+                int[] indices;
+                var nums_hash = new System.Collections.Generic.Dictionary<int, int>(); // num & index
+
+                // generate dictionary from array
+                for (int i = 0; i < size; i++) {
+                        int num = nums[i];
+
+                        if (!nums_hash.ContainsKey(num)) {
+                                nums_hash.Add(num, i);
+                                continue;
+                        }
+
+                        // corner case: duplicated key
+                        if (num == target / 2) {
+                                indices = new int[2] { nums_hash[num], i };
+                                return indices;
+                        }
+                }
+
+                // find the complement
+                foreach (int num in nums_hash.Keys) {
+                        int complement = target - num;
+                        if (nums_hash.ContainsKey(complement) && nums_hash[complement] != nums_hash[num]) {
+                                indices = new int[2] { nums_hash[num], nums_hash[complement] };
+                                return indices;
+                        }
+                }
+
+                throw new ArgumentException("Not found");
+        }
 }
+
