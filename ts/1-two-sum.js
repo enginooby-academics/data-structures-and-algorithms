4aabd702bc504a01e3f6d8bb856e66e80c85a8e8
unknown <enginoobz@gmail.com>
2021-06-06 22:39:22 +0200

    Initialize type definition for TypeScript

--- ts/1-two-sum.js
+++ ts/1-two-sum.js
@@ -1,30 +1,30 @@
 "use strict";
 // Time: 0(n^2)
 function twoSumBruteForce(nums, target) {
-	const size = nums.length;
-	let indices = new Array(2);
-	for (var i = 0; i < size - 1; i++) {
-		for (var j = i + 1; j < size; j++) {
-			if (nums[i] + nums[j] == target) {
-				return indices = [i, j];
-			}
-		}
-	}
-	return indices;
+        const size = nums.length;
+        let indices = new Array(2);
+        for (var i = 0; i < size - 1; i++) {
+                for (var j = i + 1; j < size; j++) {
+                        if (nums[i] + nums[j] == target) {
+                                return indices = [i, j];
+                        }
+                }
+        }
+        return indices;
 }
 ;
 function twoSumOnePassHashtable(nums, target) {
-	let indices = new Array(2);
-	let numsHashtable = new Map();
-	for (let i = 0; i < nums.length; i++) {
-		let complement = target - nums[i];
-		if (numsHashtable.has(complement)) {
-			// use non-null assertion operator (!) since we already checked the map has that key
-			return indices = [numsHashtable.get(complement), i];
-		}
-		numsHashtable.set(nums[i], i);
-	}
-	return indices;
+        let indices = new Array(2);
+        let numsHashtable = new Map();
+        for (let i = 0; i < nums.length; i++) {
+                let complement = target - nums[i];
+                if (numsHashtable.has(complement)) {
+                        // use non-null assertion operator (!) since we already checked the map has that key
+                        return indices = [numsHashtable.get(complement), i];
+                }
+                numsHashtable.set(nums[i], i);
+        }
+        return indices;
 }
 // test
 var nums = [2, 2, 8];
