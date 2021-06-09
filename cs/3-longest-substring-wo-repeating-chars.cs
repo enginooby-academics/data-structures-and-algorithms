5ab667115f1ca2405c66bfafdc37f392c4d07b40
unknown <enginoobz@gmail.com>
2021-06-09 20:50:08 +0200

    P3: C# | Sliding Window & Hashtable

--- cs/3-longest-substring-wo-repeating-chars.cs
+++ cs/3-longest-substring-wo-repeating-chars.cs
@@ -2,10 +2,31 @@ using System;
 
 public class Problem3 {
         public static void Main(string[] args) {
-                Console.Write(BruteForeSolution(""));
+                // Console.Write(CurrentSolution(""));
+                // Console.Write(CurrentSolution(" "));
+                // Console.Write(CurrentSolution("a"));
+                // Console.Write(CurrentSolution("ad"));
+                // Console.Write(CurrentSolution("abba"));
         }
 
-        public static int BruteForeSolution(string s) {
+        public static int SlidingWindow(string s) { // O(n) time & space
+                int maxLength = 0;
+                var visitedChars = new System.Collections.Hashtable(); // key: char & val: last visited index
+
+                for (int left = 0, right = 0; right < s.Length; right++) {
+                        if (visitedChars.ContainsKey(s[right])) {
+                                left = Math.Max((int)visitedChars[s[right]] + 1, left);
+                        }
+
+                        visitedChars[s[right]] = right;
+                        int currentLength = right - left + 1;
+                        maxLength = Math.Max(maxLength, currentLength);
+                }
+
+                return maxLength;
+        }
+
+        public static int BruteForeSolution(string s) { // O(n^3) time & O(1) space
                 if (s.Length <= 1) return s.Length;
 
                 int maxLength = 0;
