45a9927c8c061ea8b94a6d61a0b70e23d1a3216b
unknown <enginoobz@gmail.com>
2021-06-12 21:33:12 +0200

    P5: C# | Reverse Int

--- cs/5-palindrome-number.cs
+++ cs/5-palindrome-number.cs
@@ -2,10 +2,10 @@ using System;
 
 public class Problem5 {
         static void Main(String[] args) {
-                Console.WriteLine(Solution(12));
-                Console.WriteLine(Solution(0));
-                Console.WriteLine(Solution(-121));
-                Console.WriteLine(Solution(121));
+                // Console.WriteLine(Solution(12));
+                // Console.WriteLine(Solution(0));
+                // Console.WriteLine(Solution(-121));
+                // Console.WriteLine(Solution(121));
         }
 
         static bool StringSolution(int x) { // O(n) time - O(n) space
@@ -14,5 +14,11 @@ public class Problem5 {
                 Array.Reverse(xArray);  // O(n) time
 
                 return (xString == new String(xArray));
+        }
+
+        static bool IntSolution(int x) {   // O(log(n)) time - O(1) space
+                int reverse = 0;
+                for (int copy = x; copy != 0; copy /= 10) reverse = reverse * 10 + copy % 10;
+                return (x == reverse && x >= 0);
         }
 }
